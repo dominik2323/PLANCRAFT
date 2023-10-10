13a95f645bfcc6c42c043cbf9ff6617cd46d3186
d91 <dominik.tomcik23@gmail.com>
2023-10-10 09:29:25 +0200

    fix fonts glitching

--- hooks/useWindowSize.tsx
+++ hooks/useWindowSize.tsx
@@ -1,15 +1,19 @@
 import { useEffect, useRef, useState } from "react";
 
-export function useWindowSize() {
+export function useWindowSize(watchHeight?: boolean) {
   const initialState = { w: 0, h: 0 };
   const [dim, setDim] = useState(initialState);
   const prevDim = useRef(initialState);
 
   const handleResize = () => {
-    const newdim = { w: window.innerWidth, h: window.innerHeight };
+    const newdim = watchHeight
+      ? { w: window.innerWidth, h: window.innerHeight }
+      : { w: window.innerWidth, h: 0 };
 
-    prevDim.current = newdim;
-    setDim(newdim);
+    if (prevDim.current.w !== newdim.w || prevDim.current.h !== newdim.h) {
+      prevDim.current = newdim;
+      setDim(newdim);
+    }
   };
 
   useEffect(() => {
